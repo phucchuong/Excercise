a6b650b85d82d14fa80a1017b349fe13e2f293e7
Chuongnp <nguyenphucchuong1997@gmail.com>
2018-06-26 14:24:39 +0700

    add Q9.1, Q9.5

--- Question9/Q9.1/Source.cpp
+++ Question9/Q9.1/Source.cpp
@@ -1,14 +1,27 @@
 #include<iostream>
 #include<stdio.h>
+#include<cstdarg>
 using namespace std;
 
-void show(char *const a) {
-	printf("%u\n", a);
-	printf("%s\n", a);
+void show(char *const a, int num_param, ...) {
+	va_list marker;
+	va_start(marker, num_param);
+	if (a == "%ud") {
+		for (register int i = 0; i < num_param; i++) {
+			printf("%d ", va_arg(marker, int));
+		}
+	}
+	else {
+		for (register int i = 0; i < num_param; i++) {
+			printf("%u ", va_arg(marker, unsigned int));
+		}
+	}
+	va_end(marker);
 }
 
 int main() {
-	char *a = "chuong";
-	show(a);
+	show("%ud", 6, 5, 6, -8, -6, 5, -7);
+	printf("\n");
+	show("%sd", 6, 5, 6, -8, -6, 5, -7);
 	return 0;
 }
--- Question9/Q9.5/Source.cpp
+++ Question9/Q9.5/Source.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
 class ClassA
@@ -41,15 +41,21 @@ void main()
 	cout << A << " " << &B << " " << C << endl;
 	printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
 	Todo1(A, &B);
+	// hàm todo1 nhân các đối số là các con trỏ, 
+	//nhưng các con trỏ này là bản sao của các đối tượng truyền vào,
+	//nên khi thực hiện xong bộ nhớ được giải phóng => không thay đổi gì
 	cout << A << " " << &B << " " << C << endl;
 	printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
 	Todo2(*A, B);
+	// hàm todo2 có các đối só tuyền vào là tham chiếu => có thể thực hiện thay đổi cùng nhớ nhưng không thể thay đổi địa chỉ
 	cout << A << " " << &B << " " << C << endl;
 	printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
 	Todo3(*A, B);
+	// truyền vào tham trị => khi thực hiện xong thì bộ nhớ bị giải phóng => không thay đổi gì
 	cout << A << " " << &B << " " << C << endl;
 	printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
 	Todo4(A, C);
+	// todo4 truyền vào là tham chiếu con trỏ nên cả địa chỉ và cùng nhớ điều bị thay đổi.
 	cout << A << " " << &B << " " << C << endl;
 	printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
 }
